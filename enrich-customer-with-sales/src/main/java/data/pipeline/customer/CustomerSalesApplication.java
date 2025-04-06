f38eaedaff0181366b0c323e8bdab44151421d97
Ravi Chiripurapu <ravi.chiripurapu@toyota.com>
2025-04-05 23:43:31 -0500

    first commit

--- enrich-customer-with-sales/src/main/java/data/pipeline/customer/CustomerSalesApplication.java
+++ enrich-customer-with-sales/src/main/java/data/pipeline/customer/CustomerSalesApplication.java
@@ -1,13 +1,29 @@
 package data.pipeline.customer;
 
+import data.pipeline.api.error.FlowException;
+import data.pipeline.executor.cli.CommandLineOptions;
 import data.pipeline.executor.main.SparkDataPipeline;
 
 public class CustomerSalesApplication {
     public static void main(String args[]) {
+
+        String fileDirectory = System.getProperty("user.dir")+ "/enrich-customer-with-sales/src/main/resources/";
+        String appYml = fileDirectory + "application.yml";
+        String jobJson = fileDirectory + "job.json";
+        String pipeline = fileDirectory + "pipeline.json";
+        String repoYml = fileDirectory + "repo.yml";
+
+        String args1[] = {
+                String.format("-%s", CommandLineOptions.APP_YML), appYml,
+                String.format("-%s", CommandLineOptions.JOB_JSON), jobJson,
+                String.format("-%s", CommandLineOptions.PIPELINE_JSON), pipeline,
+                String.format("-%s", CommandLineOptions.REPO_YML), repoYml
+        };
+
         try {
-            SparkDataPipeline.startApplication(args);
-        } catch (data.pipeline.api.error.FlowException e) {
-            throw new RuntimeException(e);
+            SparkDataPipeline.startApplication(args1);
+        } catch (FlowException fe) {
+
         }
     }
 }
